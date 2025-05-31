59f5bd96630593c41a273920b0f6f1f8e2fcb437
LuizFernado110 <luizloona@gmail.com>
2025-05-31 10:47:51 -0300

    feature: Adicionado rotas para adição de contador em hábitos, tanto positivo como negativo. E rota para marcar tarefa como feita ou desfazer.

--- fitCore/api/urls.py
+++ fitCore/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ToDoViewSet, HabitViewSet
+from .views import ToDoViewSet, HabitViewSet, HabitAddPositiveCountView, HabitAddNegativeCountView, ToDoToggleMarkView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -9,4 +9,16 @@ router.register(r'habit', HabitViewSet, basename='habit')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('habits/<int:pk>/add-positive-counter/', 
+         HabitAddPositiveCountView.as_view(), 
+         name="habit-add-positive-counter"
+    ),
+    path('habits/<int:pk>/add-negative-counter/', 
+         HabitAddNegativeCountView.as_view(), 
+         name="habit-add-negative-counter"
+    ),
+    path('todo/<int:pk>/done-toggle/',
+        ToDoToggleMarkView.as_view(),
+        name="todo-done-toggle"
+    ),
 ]
--- fitCore/api/views.py
+++ fitCore/api/views.py
@@ -1,7 +1,8 @@
-from .serializers import TodoSerializer,HabitSerializer
+from .serializers import TodoSerializer, HabitSerializer
 from ..models import ToDo, Habit
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import UpdateAPIView
 
 class ToDoViewSet(ModelViewSet):
     serializer_class=TodoSerializer
@@ -50,3 +51,48 @@ class HabitViewSet(ModelViewSet):
     def get_queryset(self):
         queryset = Habit.objects.filter(user_id = self.request.user.id)
         return queryset
+
+
+class HabitAddPositiveCountView(UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+        
+        if instance.user_id != self.request.user.id:
+            raise PermissionDenied("Você não tem permissão para editar essa tarefa!")
+        
+        if instance.positive == False:
+            raise ValidationError("Não é possível adicionar, pois o hábito é negativo.")
+
+        serializer.save(positive_count=instance.positive_count + 1)
+
+
+class HabitAddNegativeCountView(UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+        
+        if instance.user_id != self.request.user.id:
+            raise PermissionDenied("Você não tem permissão para editar essa tarefa!")
+        
+        if instance.negative == False:
+            raise ValidationError("Não é possível adicionar, pois o hábito é positivo.")
+
+        serializer.save(negative_count=instance.negative_count + 1)
+
+
+class ToDoToggleMarkView(UpdateAPIView):
+    serializer_class = TodoSerializer
+    queryset = ToDo.objects.all()
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+
+        if instance.user_id != self.request.user.id:
+            raise PermissionDenied("Você não tem permissão para editar essa tarefa!")
+        
+        serializer.save(done=not instance.done)
