19d0ca96a7f31374b74fb5b6d70400dd569c1c03
Linkzandro <lizandrolgui123@gmail.com>
2025-06-01 14:17:55 -0300

    refactor:  adicionado ordering aos models

--- fitCore/models.py
+++ fitCore/models.py
@@ -1,19 +1,30 @@
 from django.db import models
+from datetime import datetime
 
 class ToDoBase(models.Model):
-    tittle=models.CharField(max_length=50)
+    title=models.CharField(max_length=50)
     user_id=models.PositiveIntegerField()
     description=models.TextField()
+    created=models.DateTimeField(auto_now_add=True)
 
     class Meta: 
         abstract=True
+        ordering=["-created"]
+        
 
 class ToDo(ToDoBase):
     done=models.BooleanField(default=False)
-    everyday= models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering=["done","-created"]
 class Habit(ToDoBase):
     positive=models.BooleanField(default=False)
     negative=models.BooleanField(default=False)
     positive_count=models.PositiveIntegerField(default=0)
     negative_count=models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
