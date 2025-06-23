9f42f1a2c57942c0c52d39dc4a6b1b9234b98e58
Linkzandro <lizandrolgui123@gmail.com>
2025-06-23 00:57:54 -0300

    fix: cron every midnight

--- dbDocker/start.sh
+++ dbDocker/start.sh
@@ -3,7 +3,7 @@ set -e
 
 chmod +x /docker-entrypoint-initdb.d/backup_script.sh
 
-echo "* * * * * root /docker-entrypoint-initdb.d/backup_script.sh >> /var/log/cron.log 2>&1" >> /etc/crontab
+echo "0 0 * * * root /docker-entrypoint-initdb.d/backup_script.sh >> /var/log/cron.log 2>&1" >> /etc/crontab
 
 service cron start
 
