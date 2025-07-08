4c7ed3c612d64b5b36e2a274f8e590d46c80ffba
Linkzandro <lizandrolgui123@gmail.com>
2025-07-08 08:46:09 -0300

    feat: corsheaders added

---
+++ fitDash/settings/instaledappsconf.py
@@ -0,0 +1,27 @@
+
+INSTALLED_APPS = []
+
+DEFAULT=[
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+FIRST_PARTY=[
+    'fitCore.apps.FitcoreConfig'
+]
+
+THIRD_PARTY=[    
+    'corsheaders',
+    'rest_framework',
+    'simple_history',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+]
+
+INSTALLED_APPS+=DEFAULT
+INSTALLED_APPS+=FIRST_PARTY
+INSTALLED_APPS+=THIRD_PARTY
