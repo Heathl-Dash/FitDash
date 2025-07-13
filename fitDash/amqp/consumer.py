6357a02dde20e2e6fa2778272ee1d9fe87e43698
Linkzandro <lizandrolgui123@gmail.com>
2025-07-12 23:10:56 -0300

    feat: tratando exception do consumer do rabbitMQ

--- fitDash/amqp/consumer.py
+++ fitDash/amqp/consumer.py
@@ -1,8 +1,11 @@
 import os
 import pika
 import json
+from time import sleep
 import django
+from pika.exceptions import AMQPConnectionError
 from fitCore.models import Habit, ToDo 
+
 RABBITMQ_DEFAULT_HOST = os.getenv("RABBITMQ_DEFAULT_HOST")
 RABBITMQ_DEFAULT_USER = os.getenv("RABBITMQ_DEFAULT_USER")
 RABBITMQ_DEFAULT_PASS = os.getenv("RABBITMQ_DEFAULT_PASS")
@@ -30,27 +33,36 @@ def __get_connection_and_channel():
 
 
 def start_delete_user_objects():
-    _,chan=__get_connection_and_channel()
-
-    chan.exchange_declare(exchange=RABBITMQ_EXCHANGE_NAME, exchange_type=RABBITMQ_EXCHANGE_TYPE)
-
-    chan.queue_declare(RABBITMQ_QUEUE_NAME,durable=True)
-    chan.queue_bind(exchange=RABBITMQ_EXCHANGE_NAME,queue=RABBITMQ_QUEUE_NAME)
-
-    def callback(ch,method,properties,body):
-        print('mensagem recebida')
+    retry_timer=5
+    while True:
         try:
-            data=json.loads(body)
-            user_id=data.get("user_id")
-            if user_id is not None:
-                Habit.objects.filter(user_id=user_id).delete()
-                ToDo.objects.filter(user_id=user_id).delete()
-        except Exception as e:
-            print(f'Erro: {e}')
-    chan.basic_consume(
-        queue=RABBITMQ_QUEUE_NAME,
-        on_message_callback=callback,
-        auto_ack=True
-    )
+            _,chan=__get_connection_and_channel()
 
-    chan.start_consuming()
+            chan.exchange_declare(exchange=RABBITMQ_EXCHANGE_NAME, exchange_type=RABBITMQ_EXCHANGE_TYPE)
+
+            chan.queue_declare(RABBITMQ_QUEUE_NAME,durable=True)
+            chan.queue_bind(exchange=RABBITMQ_EXCHANGE_NAME,queue=RABBITMQ_QUEUE_NAME)
+
+            def callback(ch,method,properties,body):
+                print('mensagem recebida')
+                try:
+                    data=json.loads(body)
+                    user_id=data.get("user_id")
+                    if user_id is not None:
+                        Habit.objects.filter(user_id=user_id).delete()
+                        ToDo.objects.filter(user_id=user_id).delete()
+                except Exception as e:
+                    print(f'Erro: {e}')
+            chan.basic_consume(
+                queue=RABBITMQ_QUEUE_NAME,
+                on_message_callback=callback,
+                auto_ack=True
+            )
+
+            chan.start_consuming()
+        except AMQPConnectionError as e:
+            print(f"falha de conexão com rabbitMQ: {e}, reiniciando conexão")
+            sleep(retry_timer)
+        except Exception as e:
+            print(f"falha inesperada: {e}, reiniciando conexão")
+            sleep(retry_timer)
