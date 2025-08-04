02b8b4c8c8cd27714d4ad064c177deb301373cca
LuizFernado110 <luizloona@gmail.com>
2025-08-04 14:39:13 -0300

    Fix: Descrição ser opcional em tarefas e hábitos.

--- fitCore/serializers/todo_serializer.py
+++ fitCore/serializers/todo_serializer.py
@@ -4,6 +4,7 @@ from ..models import ToDo
 
 class TodoSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(read_only=True)
+    description = serializers.CharField(allow_blank=True, required= False)
 
     class Meta:
         model = ToDo
