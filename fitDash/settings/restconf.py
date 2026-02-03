4521bdf8cbade7aa7a87c41dc8a96475108835dc
Linkzandro <lizandrolgui123@gmail.com>
2026-02-03 18:35:58 -0300

    feat: pagination for tests

--- fitDash/settings/restconf.py
+++ fitDash/settings/restconf.py
@@ -3,6 +3,8 @@ REST_FRAMEWORK = {
         "fitCore.api.permission_classes.DashboardJWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
